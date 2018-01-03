14a13982e48d0d513755624c181475fb20d99c85
MonsieurMan <ange.g.picard@gmail.com>
2018-01-03 08:21:32 +0100

    Fix build

--- src/core/decorators/option/option-metadata.ts
+++ src/core/decorators/option/option-metadata.ts
@@ -2,7 +2,7 @@
  * Configuration for the `Option` decorator
  */
 export interface OptionMetadata {
-    name?: string;
+    name?: string | symbol;
     alias?: string[];
     brief?: string;
     description?: string;
